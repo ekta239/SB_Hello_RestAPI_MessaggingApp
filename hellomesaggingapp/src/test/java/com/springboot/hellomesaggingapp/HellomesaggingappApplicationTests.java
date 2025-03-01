18c1d6121f44988f7e8adc372af4b318550c36da
Ekta verma <ektav8231@gmail.com>
2025-03-01 13:46:11 +0530

    [Ekta] Add.create HelloMessagingApp and REST controller to demonstrate various methods

---
+++ hellomesaggingapp/src/test/java/com/springboot/hellomesaggingapp/HellomesaggingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hellomesaggingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellomesaggingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
